29b9bcf6bb61373f1e1e958af30259653bc3e000
MangoMustard6 <liamxandertamondong@gmail.com>
2026-05-30 19:52:24 +0800

    Change command prefix from '!' to 'bop!'

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="bop!", intents=intents)
 
 # ================= STATE =================
 auto_reply_enabled = False
